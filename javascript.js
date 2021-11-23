8c8ce115d7a9f9cdcd08d11288e68d284564a632
droplotus <joaobpedrof@gmail.com>
2021-11-23 12:02:30 +0000

    coiso

--- javascript.js
+++ javascript.js
@@ -1,27 +1,13 @@
 // run this function when the document is loaded
 window.onload = function() {
 
-
+	var midmid;
 	const outer = document.createElement("div");
 	document.body.appendChild(outer);
 
-	function createMidDiv() {
- 		const mid = document.createElement("div");
- 		outer.appendChild(mid);
-
- 		mid.style.backgroundColor = "purple";
- 		mid.style.flexGrow = "1";
-		mid.style.paddingTop = "1em";
-		mid.style.paddingBottom = "1em";
-		mid.style.margin = "1em";
-		mid.style.border = "1em";
-
-	}
-
 	outer.style.backgroundColor = "red";
 	outer.style.height = "500px";
-	outer.style.width = "70%"
-	outer.style.margin = "30px";
+	outer.style.width = "70%";
 
 	outer.style.display = "flex";
 	outer.style.justifyContent = "space-around";
@@ -29,8 +15,50 @@ window.onload = function() {
 	outer.style.marginLeft = "auto";
 	outer.style.marginRight = "auto";
 
+	function createMidDiv() {
+ 		const mid = document.createElement("div");
+ 		outer.appendChild(mid);
+
+ 		mid.style.display = "flex";
+ 		mid.style.backgroundColor = "purple";
+ 		mid.style.flexGrow = "1";
+		mid.style.paddingTop = "1em";
+		mid.style.paddingBottom = "1em";
+		mid.style.margin = "1em";
+		mid.style.border = "1em";
+	}
+
+	function createMidMidDiv() {
+		midmid = document.createElement("div");
+ 		outer.appendChild(midmid);
+
+ 		midmid.style.display = "flex";
+ 		midmid.style.backgroundColor = "green";
+ 		midmid.style.flexGrow = "1";
+		midmid.style.paddingTop = "1em";
+		midmid.style.paddingBottom = "1em";
+		midmid.style.margin = "auto";
+		midmid.style.border = "1em";
+	}
+
+	function createInsideMidMid() {
+		const inside = document.createElement("div");
+ 		midmid.appendChild(inside);
+
+ 		inside.style.display = "flex";
+ 		inside.style.backgroundColor = "black";
+ 		inside.style.flexGrow = "1";
+		inside.style.paddingTop = "1em";
+		inside.style.paddingBottom = "1em";
+		inside.style.margin = "2em";
+		inside.style.border = "1em";
+	}
+
 	createMidDiv();
-	createMidDiv();
+	createMidMidDiv();
+
+	for (let i = 0; i < 6; i++) createInsideMidMid();
+	
 	createMidDiv();
 
 }
