98861fc167174fcafc4fda95bdf055582fe8ff6c
droplotus <joaobpedrof@gmail.com>
2021-11-23 11:41:08 +0000

    sakdna

--- javascript.js
+++ javascript.js
@@ -22,11 +22,39 @@ window.onload = function() {
 	mid_two.appendChild(inside_mid_two);
 
 	outer.style.backgroundColor = "red";
-	outer.style.height = "400px";
+	outer.style.height = "500px";
+	outer.style.width = "70%"
 	outer.style.margin = "30px";
 
+	outer.style.display = "flex";
+	outer.style.justifyContent = "space-around";
+	outer.style.marginTop = "2em";
+	outer.style.marginLeft = "auto";
+	outer.style.marginRight = "auto";
+
+
+	mid_one.style.backgroundColor = "purple";
+	mid_two.style.backgroundColor = "green";
+	mid_three.style.backgroundColor = "purple";
+
+	mid_one.style.flexGrow = "1";
+	mid_one.style.paddingTop = "1em";
+	mid_one.style.paddingBottom = "1em";
+	mid_one.style.margin = "1em";
+	mid_one.style.border = "1em";
+
+	mid_two.style.flexGrow = "1";
+	mid_two.style.paddingTop = "1em";
+	mid_two.style.paddingBottom = "1em";
+	mid_two.style.margin = "1em";
+	mid_two.style.border = "1em";
+
+	mid_three.style.flexGrow = "1";
+	mid_three.style.paddingTop = "1em";
+	mid_three.style.paddingBottom = "1em";
+	mid_three.style.margin = "1em";
+	mid_three.style.border = "1em";
 
 	document.body.appendChild(outer);
-
 
 }
