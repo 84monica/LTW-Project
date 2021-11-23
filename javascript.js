2d9d2d45939ff54aaea8abf300f481adf88582cd
droplotus <joaobpedrof@gmail.com>
2021-11-23 11:48:01 +0000

    added function

--- javascript.js
+++ javascript.js
@@ -3,23 +3,20 @@ window.onload = function() {
 
 
 	const outer = document.createElement("div");
-	const mid_one = document.createElement("div");
-	const mid_two = document.createElement("div");
-	const mid_three = document.createElement("div");
-	const inside_mid_two = document.createElement("div");
+	document.body.appendChild(outer);
 
-	outer.appendChild(mid_one);
-	outer.appendChild(mid_two);
-	outer.appendChild(mid_three);
+	function createMidDiv() {
+ 		const mid = document.createElement("div");
+ 		outer.appendChild(mid);
 
-	//  -----------------------------------
-	// |  ///////     ///////     ///////  |
-	// |  /     /     /     /     /     /  |
-	// |  /     /     /  /  /     /     /  |
-	// |  /     /     /     /     /     /  |
-	// |  ///////     ///////     ///////  |
-	//  -----------------------------------
-	mid_two.appendChild(inside_mid_two);
+ 		mid.style.backgroundColor = "purple";
+ 		mid.style.flexGrow = "1";
+		mid.style.paddingTop = "1em";
+		mid.style.paddingBottom = "1em";
+		mid.style.margin = "1em";
+		mid.style.border = "1em";
+
+	}
 
 	outer.style.backgroundColor = "red";
 	outer.style.height = "500px";
@@ -32,29 +29,8 @@ window.onload = function() {
 	outer.style.marginLeft = "auto";
 	outer.style.marginRight = "auto";
 
-
-	mid_one.style.backgroundColor = "purple";
-	mid_two.style.backgroundColor = "green";
-	mid_three.style.backgroundColor = "purple";
-
-	mid_one.style.flexGrow = "1";
-	mid_one.style.paddingTop = "1em";
-	mid_one.style.paddingBottom = "1em";
-	mid_one.style.margin = "1em";
-	mid_one.style.border = "1em";
-
-	mid_two.style.flexGrow = "1";
-	mid_two.style.paddingTop = "1em";
-	mid_two.style.paddingBottom = "1em";
-	mid_two.style.margin = "1em";
-	mid_two.style.border = "1em";
-
-	mid_three.style.flexGrow = "1";
-	mid_three.style.paddingTop = "1em";
-	mid_three.style.paddingBottom = "1em";
-	mid_three.style.margin = "1em";
-	mid_three.style.border = "1em";
-
-	document.body.appendChild(outer);
+	createMidDiv();
+	createMidDiv();
+	createMidDiv();
 
 }
