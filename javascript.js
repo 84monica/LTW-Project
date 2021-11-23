937da8501df4d2f34bb29f62517094837e28656e
84monica <up201905753@fe.up.pt>
2021-11-23 12:28:11 +0000

    added class board

--- javascript.js
+++ javascript.js
@@ -1,64 +1,70 @@
 // run this function when the document is loaded
 window.onload = function() {
+	class Board {
+		constructor() {
+			this.midmid;
+			this.outer = document.createElement("div");
+			document.body.appendChild(this.outer);
+		}
+		createOuterDiv() {
+			this.outer.style.backgroundColor = "red";
+			this.outer.style.height = "500px";
+			this.outer.style.width = "70%";
 
-	var midmid;
-	const outer = document.createElement("div");
-	document.body.appendChild(outer);
+			this.outer.style.display = "flex";
+			this.outer.style.justifyContent = "space-around";
+			this.outer.style.marginTop = "2em";
+			this.outer.style.marginLeft = "auto";
+			this.outer.style.marginRight = "auto";
+		}
 
-	outer.style.backgroundColor = "red";
-	outer.style.height = "500px";
-	outer.style.width = "70%";
+		createMidDiv() {
+			const mid = document.createElement("div");
+			this.outer.appendChild(mid);
+   
+			mid.style.display = "flex";
+			mid.style.backgroundColor = "purple";
+			mid.style.flexGrow = "1";
+			mid.style.paddingTop = "1em";
+			mid.style.paddingBottom = "1em";
+			mid.style.margin = "1em";
+			mid.style.border = "1em";
+	   }
 
-	outer.style.display = "flex";
-	outer.style.justifyContent = "space-around";
-	outer.style.marginTop = "2em";
-	outer.style.marginLeft = "auto";
-	outer.style.marginRight = "auto";
+	   createMidMidDiv() {
+			this.midmid = document.createElement("div");
+			this.outer.appendChild(this.midmid);
 
-	function createMidDiv() {
- 		const mid = document.createElement("div");
- 		outer.appendChild(mid);
+			this.midmid.style.display = "flex";
+			this.midmid.style.backgroundColor = "green";
+			this.midmid.style.flexGrow = "1";
+			this.midmid.style.paddingTop = "1em";
+			this.midmid.style.paddingBottom = "1em";
+			this.midmid.style.margin = "auto";
+			this.midmid.style.border = "1em";
+		}
 
- 		mid.style.display = "flex";
- 		mid.style.backgroundColor = "purple";
- 		mid.style.flexGrow = "1";
-		mid.style.paddingTop = "1em";
-		mid.style.paddingBottom = "1em";
-		mid.style.margin = "1em";
-		mid.style.border = "1em";
+		createInsideMidMid() {
+			const inside = document.createElement("div");
+			this.midmid.appendChild(inside);
+
+			inside.style.display = "flex";
+			inside.style.backgroundColor = "black";
+			inside.style.flexGrow = "1";
+			inside.style.paddingTop = "1em";
+			inside.style.paddingBottom = "1em";
+			inside.style.margin = "2em";
+			inside.style.border = "1em";
+		}
 	}
 
-	function createMidMidDiv() {
-		midmid = document.createElement("div");
- 		outer.appendChild(midmid);
+	this.board = new Board();
+	this.board.createOuterDiv();
+	this.board.createMidDiv();
+	this.board.createMidMidDiv();
 
- 		midmid.style.display = "flex";
- 		midmid.style.backgroundColor = "green";
- 		midmid.style.flexGrow = "1";
-		midmid.style.paddingTop = "1em";
-		midmid.style.paddingBottom = "1em";
-		midmid.style.margin = "auto";
-		midmid.style.border = "1em";
-	}
-
-	function createInsideMidMid() {
-		const inside = document.createElement("div");
- 		midmid.appendChild(inside);
-
- 		inside.style.display = "flex";
- 		inside.style.backgroundColor = "black";
- 		inside.style.flexGrow = "1";
-		inside.style.paddingTop = "1em";
-		inside.style.paddingBottom = "1em";
-		inside.style.margin = "2em";
-		inside.style.border = "1em";
-	}
-
-	createMidDiv();
-	createMidMidDiv();
-
-	for (let i = 0; i < 6; i++) createInsideMidMid();
+	for (let i = 0; i < 6; i++) this.board.createInsideMidMid();
 	
-	createMidDiv();
+	this.board.createMidDiv();
 
 }
